b584b3061872e3dcf28c19d35a2675af807a02e2
William Seed <wseed@caci.com>
2014-06-20 18:55:25 -0400

    Screw change management... Huge commit

--- openwrt.py
+++ openwrt.py
@@ -3,52 +3,135 @@
 import pexpect
 import random
 import time
+import string
+import re
+from collections import deque
 
+######################################################
+# pexpect must be used in the thread that created it #
+######################################################
 class QemuImage(object):
+    initialized = False
+    inuse = False
+    users = 0
     name = ''       # snapshot name
     router = None   # qemu pexpect instance
+    qemuprompt = 'root@OpenWrt:.+\\#'
+    pwd = '/'
+    interrupt = 0
+    lines = deque()
 
     def __init__(self, memory=30, snapshot=''):    # memory in megabytes, snapshot is string
         start_time = time.time()
         self.name = snapshot
-        if self.snapshot is '':   # we'll create a new one
-            self.name = str(int(time.time())) + random_string() + '.qcow2'  # timestampSTRING.qcow2
+        self.memory = memory
+        if self.name == '':   # we'll create a new one
+            # self.name = str(int(time.time())) + random_string() + '.qcow2'  # timestampSTRING.qcow2
+            self.name = '%s-%s.qcow2' % (str(int(time.time())), random_string())
             cmd = 'qemu-img create -b base.qcow2 -f qcow2 %s' % self.name
             print '[#] running command: ' + cmd
             creator = pexpect.spawn(cmd)
             creator.expect(pexpect.EOF)
             for line in creator.before.split('\n'):
-                print '[#] ' + line     # let's use print for logging
+                print '[#] %s' % line     # let's use print for logging
             # fyi: http://stackoverflow.com/questions/17067560/intercept-pythons-print-statement-and-display-in-gui
             time.sleep(0.01)    # http://pexpect.readthedocs.org/en/latest/commonissues.html#timing-issue-with-isalive
             if creator.isalive():
                 print '[-] looks like something went wrong - why is the creator alive?'
             else:
                 print '[+] created new image: %s' % self.name
-                print '[+] done in %s seconds' % str(time.time() - start_time)
+                print '[#] done in %s seconds' % str(time.time() - start_time)
         else:
             print '[+] using previous image: %s' % self.name
+
+    def boot(self):
         boot_time = time.time()
-        cmd = 'qemu-system-i386 -net nic,model=ne2k_pci -nographic -m %sM -hda %s' % self.memory, self.name
+        print '[#] Now we wait...'
+        cmd = 'qemu-system-i386 -net nic,model=ne2k_pci -nographic -m %sM -hda %s' % (self.memory, self.name)
+        print '[#] executing: %s' % cmd
         self.router = pexpect.spawn(cmd)
-        self.router.expect('Please press Enter to activate this console.')
+        try:
+            self.router.expect('activate this console.', timeout=40)
+        except:
+            print 'Exception expecting \"activate this console\"...'
+            print str(self.router)
+            exit()
         self.router.sendline()
+        print '[#] Pressed enter'
+        try:
+            # Yes, there are three of them
+            self.router.expect('entered forwarding state', timeout=30)
+            self.router.expect('entered forwarding state', timeout=30)
+            self.router.expect('entered forwarding state', timeout=30)
+        except:
+            print 'Exception expecting \"entered forwarding state\"'
+            print str(self.router)
         self.router.setecho(False)
-        print '[+] boot finished in %s seconds' % boot_time - time.time()
-        if boot_time - time.time() > 30:
-            print '[-] %s seconds is a long time... what\'s up?' % boot_time - time.time()
+        print '[+] boot finished in %s seconds' % str(time.time() - boot_time)
+        self.initialized = True
+        if boot_time - time.time() > 60:
+            print '[-] %s seconds is a long time... what\'s up?' % str(boot_time - time.time())
 
     def execute(self, cmd):
+        self.router.sendline()
+        self.router.expect(self.qemuprompt, timeout=1)
+        if cmd == 'poweroff':
+            return self.poweroff()
+        if cmd == 'exit':
+            # TODO: set timer to power off
+            return 'close'
         self.router.sendline(cmd)
+        return self.process_output(cmd)
+
+    def process_output(self, cmd):
+        while self.interrupt == 0:
+            timeout = self.router.expect(['\r\n', pexpect.TIMEOUT], timeout=.2)
+            out = self.router.before
+            for line in out.split('\r\n'):     # sanitize
+                if cmd in line or 'OpenWrt' in line:
+                    if 'not found' in line:
+                        self.lines.append(line)
+                    continue
+                self.lines.append(line)
+            if timeout == 1:
+                if 'root@OpenWrt' in self.router.before:
+                    self.pwd = re.search(r'OpenWrt:(.+)#', self.router.before).group(1)
+                    return 'done'
+                    # this^ returns the 'status' and sets 'pwd'
+                else:
+                    # TODO: Maybe call a function to check if we should keep looping
+                    # hm, maybe I'll just make a garbage collector
+
+                    return 'waiting'
+        return 'interrupted'
+
+    def poweroff(self):
+        self.router.sendline('poweroff')
+        self.router.expect(pexpect.EOF)
+        self.initialized = False
+        print '[!] Instance %s powered off' % self.name
+        return 'close'
+
+    def die(self):
+        self.poweroff()
+        if not self.inuse:  # delete if never used
+            cmd = 'rm %s' % self.name
+            rmchild = pexpect.spawn(cmd)
+            rmchild.expect(pexpect.EOF)
+            print '[#] Instance %s deleted successfully' % self.name
+        self.initialized = False
 
 '''
-    TODO: Find a good way to filter actor's input and output we probably want 
+    TODO: Find a good way to filter actor's input and output. we probably want 
     to make a list of "easy" commands (commands that execute, print to stdout, 
     then exit) so we can execute those without trouble.
     We'll need to go into router.interact() mode when the actor is doing
     anything besides these easy commands. To do that without him/her noticing,
     we'll need to filter it, and maybe insert an escape token if something bad
     happens or when the necessity for interact() is gone.
+
+    BUT, *maybe* - if we can't find any better way to do this stuff. This is 
+    probably the most "thin" way, but it's more difficult
 '''
 def input_filter(data):
     pass    
@@ -59,3 +142,31 @@ def output_filter(data):
 def random_string(number=1):  # number: how many string names
     return ''.join([random.choice(string.letters) for i in xrange(10)])
     # I hope 10 chars + timestamp is enough to be always unique...
+
+if __name__ == '__main__':
+    print '[!] Starting openwrt class test'
+    shell = QemuImage()
+    user = raw_input('user: ')
+    raw_input('pass: ')         # TODO: we should append the supplied username/passwd to /etc/passwd
+    while True:
+        cmd = raw_input('%s@thing:%s# ' % (user,shell.pwd))
+        result = shell.execute(cmd)
+        if result == 'close':
+            print '[+] done'
+            break
+        elif result == 'waiting':
+            print '[!] shouldn\'t be waiting for anything...'
+        while shell.lines:
+            print shell.lines.popleft()
+
+
+
+''' TODO:
+
+There are a bunch of things we'll want to do to make the environment look like a router...
+
+/proc/cpuinfo
+username+password in /etc/passwd
+
+
+'''
--- server.py
+++ server.py
@@ -5,83 +5,204 @@ import threading
 import SocketServer
 import signal
 import sys
+import time
+from collections import deque
+from openwrt import QemuImage
 
-# yes, ^C is passed to thread (somehow)
+EXTRA_HOSTS = 3     # number of qemu instances to keep sitting idle, waiting for connections
+queuemu = deque()
+dequelock = 0       # the 'for x in deque()' can take a while, so we should make it thread-safe
+lockuser = ''
 
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+    shell = None
 
     def handle(self):
+        global dequelock, lockuser
+        try:
+            qlock('handle')
+            lockuser = 'handle'
+            self.shell = queuemu.popleft()
+        except: # queuemu is empty. hopefully this is < 40 seconds after program start
+            print '[!] User attempted to connect. Probably (hopefully) during initial boot'
+            print '[#] Otherwise, consider setting EXTRA_HOSTS higher'
+            self.request.send('\r\nBooting... try again in a minute or so\r\n\r\n')
+            self.request.close()
+            return
+        finally:
+            qrelease()
+        user, password = self.do_login()
+        self.process_commands(user, password)
+
+    def do_login(self):
+        self.request.sendall('It\'s a telnet server! Not yours, go away.\r\n')
+        self.request.sendall('Username: ')
+        user = self.request.recv(1024).rstrip()
+        self.request.sendall('Password: ')
+        password = self.request.recv(1024).rstrip()
+        return user, password
+
+    # my very own REPL (read-eval-print loop)
+    def process_commands(self, user, password):
         while True:
+            self.request.sendall('%s@thing:%s# ' % (user,self.shell.pwd))
             string = ''
-            self.request.sendall('# ')
-            while True: # interrupts and telnet commands are sent immediately
-                data = self.request.recv(1)
+            while True: # interrupts/telnet commands are sent immediately
+                data, result = self.receive_loop()
                 string += data
-                if data == '':
-                    print '[+] breaked due to data = \'\''
+                if result == 'break':
                     break
-                if ord(data) == 6:  # ^C
-# We get IAC IP IAC DO 0x06  (interrupt, then confirm (WILL) you'll do 0x06 (ACK), then ACK (0x06))
-                    self.request.send(chr(0xff))    # IAC (0xff)
-                    self.request.send(chr(0xfb))    # WILL (0xfb)
-                    self.request.send(chr(0x06))    # ACK
-                    self.request.sendall('\r\n<interrupt>\r\n')
-                    # GA (249 ... 0xf9)     # BRK (0xf3)    # EC (0xf7)
-    # we should maybe implement telnet commands? advanced actors might check
+                elif result == 'cmd:interrupt':
+                    string = 'cmd:interrupt'
+                    break
+            result = self.check_cmd_loop(string)
+            if result == 'break':
+                break
+            elif result == 'continue':
+                continue
+            elif result == 'good':
+                # done checking - now execute!
+                result = self.shell.execute(string)
+                if result == 'close':
+                    print '[+] done'
+                    break
+                elif result == 'waiting':
+                    print '[!] shouldn\'t be waiting for anything...'
+                first = True
+                while self.shell.lines:
+                    tosend = self.shell.lines.popleft().lstrip().rstrip()
+                    if tosend == '':
+                        continue
+                    self.request.sendall(tosend + '\r\n')
+                continue
+            elif result == 'cmd:myname':
+                self.request.send(self.shell.name + '\r\n')
+                continue
+            print '[!!!] WE SHOULD NEVER VENTURE HERE'
+
+    def receive_loop(self):
+        data = self.request.recv(1)
+        if data == '':
+            print '[+] breaked due to data == \'\''
+            return data, 'break'
+        if ord(data) == 6:  # ^C
+            # We get IAC IP IAC DO 0x06
+            self.request.send(chr(0xff))    # IAC (0xff)
+            self.request.send(chr(0xfb))    # WILL (0xfb)
+            self.request.send(chr(0x06))    # ACK
+            self.request.sendall('\r\n<interrupt>\r\n')
+            # GA (0xf9)     # BRK (0xf3)    # EC (0xf7) # don't need these (yet?)
+# we should maybe implement telnet commands? advanced actors might check
 # http://mars.netanya.ac.il/~unesco/cdrom/booklet/HTML/NETWORKING/node300.html (not 100% accurate)
-# http://www.ietf.org/rfc/rfc854.txt (accurate command list)
+# http://www.ietf.org/rfc/rfc854.txt (accurate command list > 240)
 # http://tools.ietf.org/html/rfc1184 (contains < 240)
-                    break
-                if ord(data) == 4:  # ^D
-                    self.request.sendall('logoff\r\n')
-                    break
-                if ord(data) == 0x0d and ord(self.request.recv(1)) == 0x0a: # \r\n
-                    break
-            if string == '':
-                print '[#] Empty string - we\'re done'
-                break
-            if chr(6) in string:
-                print '[+] processing ^C (continuing)'
-                # TODO: process ^C
-                continue 
-            if chr(4) in string:
-                print '[!] got ^D, closing'
-                break
-            # TODO: execute string
-            # TODO: send response back (streaming?)
-            print '[+] got string: %s' % string
-            if 'die' in string:
-                print '[!!!] ded'
-                def die():
-                    server.shutdown()
-                threading.Thread(target=die).start()
-                self.request.close()
-                break
+            return data, 'cmd:interrupt'
+        if ord(data) == 4:  # ^D
+            self.request.sendall('logoff\r\n')  # it looks legit?
+            return data, 'break'
+        if ord(data) == 0x0d and ord(self.request.recv(1)) == 0x0a: # \r\n
+            return data, 'break'   # let's hope they don't send a \r by itself...
+        elif ord(data) == 0x0d: # but let's log it anyways and pretend they sent \n
+            print '[!!!] THEY SENT A \\r WITHOUT A \\n!'
+            return data, 'break'
+        else:
+            return data, 'good'
+
+    def check_cmd_loop(self, string):
+        print '[+] got string: %s' % string
+        if string == '':
+            print '[!!] Empty string - we\'re done... why'
+            return 'break'
+        elif string == 'cmd:interrupt':
+            print '[+] processing ^C (continuing)'
+            self.shell.execute(chr(3))
+            return 'continue' 
+        elif chr(4) in string:
+            print '[+] got ^D, closing'
+            return 'break'
+        elif 'cmd:diepot' in string:
+            print '[!] ded'
+            def die():
+                server.shutdown()
+            threading.Thread(target=die).start()
+            self.request.close()
+            return 'break'
+        elif 'cmd:myname' in string:
+            print '[!] Sending honeypot\'s name'
+            return 'cmd:myname'
+        return 'good'
+
 
 
 # source here: http://hg.python.org/cpython/file/2.7/Lib/SocketServer.py
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     allow_reuse_address = True  # so it doesn't whine about "already in use"
-    pass
+
+
+def start_qemu():
+    global queuemu, booting, dequelock, lockuser
+    print '[#] Creating new qemu instance'
+    instance = QemuImage()
+    instance.boot()
+    time.sleep(25)       # it takes a little to "truly" be ready...
+    qlock('start_qemu')
+    queuemu.append(instance)    # we should keep enough alive that 'sleep(3)' doesn't matter
+    qrelease()
+    booting -= 1
+    print '[+] machine %s is ready to go!' % instance.name
+
+# Press ^C in the terminal to print out current execution time
+def show_time(signal, frame):
+    print '[+++] Execution time: %s' % (time.time() - start_time)
+signal.signal(signal.SIGINT, show_time)
 
 server = None
+ready = False
+booting = 0
+start_time = 0.0
 
 def main():
-    global server
-    server = ThreadedTCPServer(('localhost', 0), ThreadedTCPRequestHandler)
+    global server, queuemu, ready, booting, start_time, dequelock, lockuser
+    start_time = time.time()
+    server = ThreadedTCPServer(('localhost', 62915), ThreadedTCPRequestHandler)
     ip, port = server.server_address
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.daemon = True
-    print '[+] starting server on ip %s' % str(port)
+    print '[+] starting server on port %s' % str(port)
     server_thread.start()
-    server_thread.join()
+    while server_thread.isAlive():
+        qavail = 0  # available qemu instances
+        qlock('main')
+        for qemu in queuemu:
+            if not qemu.inuse:
+                qavail += 1
+        qrelease()
+        if qavail == 0:
+            ready = False
+        for x in range(EXTRA_HOSTS - (qavail+booting)):     # 3 available at all times
+            print '[!!!] Booting image'
+            booter = threading.Thread(target=start_qemu)
+            booter.start()
+            booting += 1
+        server_thread.join(1)
+    booter.join()   # if it's killed while one is booting, it can't properly process the kill command
+    qlock('main-shutdown')
+    for instance in queuemu:
+        booter.join()   # if it's killed while one is booting, it can't properly process the kill command
+        instance.die()
+        qrelease()
 
-def sigint_handler(singal, frame):
-    global server
-    print '<> got SIGINT'
-        # pexpect.send(chr(3))  # to send SIGINT to qemu
-    # server.shutdown()
-signal.signal(signal.SIGINT, sigint_handler)
+def qlock(who): # who's trying to take the lock?
+    global lockuser, dequelock
+    while dequelock > 0:
+        time.sleep(1)
+        print '[!] dequelock held from the %s thread by %s' % (who, lockuser)
+    dequelock += 1
+    lockuser = who
+
+def qrelease():
+    global dequelock
+    dequelock -= 1
 
 if __name__ == '__main__':
     main()
